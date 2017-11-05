3205f28ba7f8f57ac3590b5cdba78651b98d2ba4
wielants <wielants@users.noreply.github.com>
2017-11-05 23:07:26 +0100

    added tests for Map interface (implementation: HashMap, TreeMap)

---
+++ src/test/java/my/java/util/MapTest.java
@@ -0,0 +1,82 @@
+package my.java.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class MapTest {
+
+	@Parameters(name="{0}")
+	public static Object[] data() {
+		return new Object[] { HashMap.class, TreeMap.class };
+	}
+
+	private Map<String, String> map;
+
+	public MapTest(Class<Map<String, String>> implClass) throws InstantiationException, IllegalAccessException {
+		map = implClass.newInstance();
+	}
+
+	@Test
+	public void createdMapIsEmpty() {
+		assertTrue(map.isEmpty());
+	}
+
+	@Test
+	public void addedMapIsNotEMpty() {
+		map.put("key", "value");
+
+		assertFalse(map.isEmpty());
+	}
+
+	@Test
+	public void addedKeyCanBeQueried() {
+		map.put("key", "value");
+
+		assertThat(map, Matchers.hasEntry("key", "value"));
+	}
+
+	@Test
+	public void addedKeyCanBeDeleted() {
+		map.put("key", "value");
+
+		map.remove("key");
+
+		assertThat(map, Matchers.not(Matchers.hasEntry("key", "value")));
+	}
+
+	@Test
+	public void clearedMapIsEmpty() {
+		map.put("key", "value");
+
+		map.clear();
+
+		assertTrue(map.isEmpty());
+	}
+
+	//FIXME: move into separate test class!
+	@Test
+	public void addNullIsAllowed() {
+		if (map instanceof TreeMap) {
+			try {
+				map.put(null, "null-value");
+				fail();
+			} catch (NullPointerException e) {
+			}
+		} else {
+			map.put(null, "null-value");
+		}
+	}
+}
